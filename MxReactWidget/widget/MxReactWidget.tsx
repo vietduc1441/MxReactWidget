ec443b5ac8eb26ca870df89d439ba8ff9e85b5cc
vietduc1441 <vietduc1441@yahoo.com>
2018-09-25 09:53:45 +0700

    add some more comments

--- MxReactWidget/widget/MxReactWidget.tsx
+++ MxReactWidget/widget/MxReactWidget.tsx
@@ -1,20 +1,40 @@
 import * as React from "react";
 import { App } from "./App";
-export default class MxReactWidget extends React.Component<{}, {}> {
-    private property: any;
-    constructor(props) {
+/**
+ * Built-in Properties of Mendix
+ */
+interface IMendixDefaultProps {
+    /** Class configured from Modeller */
+    class: string;
+    /** Context Object of the widget */
+    mxObject?: mendix.lib.MxObject;
+    /** Style configured from Modeller */
+    style: string;
+    /** Form object contains the widget */
+    mxform: mxui.lib.form._FormBase;
+}
+/**
+ * Interface for variable sent from Modeller
+ */
+interface IMxReactWidgetProps extends IMendixDefaultProps {
+    messageString?: string
+}
+/** Widget state, if changed, the widget will be re-rendered */
+interface IWidgetState {
+    messageString?: string;
+}
+export default class MxReactWidget extends React.Component<IMxReactWidgetProps, IWidgetState> {
+    constructor(props: IMxReactWidgetProps) {
         super(props);
-        this.property = {
-        };
         this.state = {
+            messageString: props.messageString
         };
     }
     componentWillMount() {
         console.log('component will mount');
     }
-    componentWillReceiveProps(nextProps: I.IEMagizFlowProps) {
-        console.log("component will receive props")
-
+    componentWillReceiveProps(nextProps: IMxReactWidgetProps) {
+        this.setState({ messageString: nextProps.messageString })
     }
 
     render() {
