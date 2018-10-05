19ee2bd3c8544fc8227da6986dd729e62bed13bc
vietduc1441 <vietduc1441@yahoo.com>
2018-10-05 13:10:03 +0700

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,15 @@ var zip = require('gulp-zip');
 var webpack = require("webpack-stream");
 var merge2 = require("merge2");
 var sequence = require('run-sequence');
+const debug = require('gulp-debug');
 
 var tsProject = ts.createProject('tsconfig.json');
 gulp.task('compileTs', function () {
     var tsResult = tsProject.src()
+        .pipe(debug({ title: "compile ts" }))
         .pipe(tsProject());
     return tsResult.js
         .pipe(gulp.dest('./release/MxReactWidget/widget'));
-
 });
 
 gulp.task("otherFiles", function () {
@@ -27,21 +28,35 @@ gulp.task("otherFiles", function () {
 });
 
 gulp.task("clean", function () {
-    return gulp.src("./release/")
-        .pipe(clean({ force: true }));
+    return merge2([
+        gulp.src("./release/")
+            .pipe(debug({ title: "Clean release/" }))
+            .pipe(clean({ force: true })),
+        gulp.src("./bundle/")
+            .pipe(debug({ title: "Clean bundle/" }))
+            .pipe(clean({ force: true })),
+    ]);
 });
 gulp.task("zip", function () {
     return gulp.src("./bundle/**/*")
         .pipe(zip("MxReactWidget.mpk"))
         .pipe(gulp.dest("./test/widgets/"))
 })
+gulp.task("copyToServer", () => {
+    return gulp.src(["./bundle/**/*.*"])
+        .pipe(gulp.dest("test/deployment/web/widgets"))
+})
 gulp.task('webpack', () =>
     gulp.src("./release/MxReactWidget/widget/MxReactWidget.js")
+        .pipe(debug({ title: "webpack" }))
         .pipe(webpack(require("./webpack.config.js")))
+        .on('error', function handleError() {
+            this.emit('end'); // Recover from errors
+        })
         .pipe(gulp.dest("./bundle"))
 )
-gulp.task("taskList", sequence(["compileTs", "otherFiles"], "webpack", "zip"));
+gulp.task("taskList", () => sequence(["otherFiles", "compileTs"], "webpack", ["zip", "copyToServer"]));
 gulp.task("watch", function () {
     gulp.watch("./MxReactWidget/**/*", ["taskList"]);
 });
-gulp.task("default", ["clean", "watch"]);
+gulp.task("default", () => sequence("clean", "watch"));
