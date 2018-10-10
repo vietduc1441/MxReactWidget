0e1d8ad3fc011da4c2db0832405e66101b9c1592
vietduc1441 <vietduc1441@yahoo.com>
2018-10-10 11:17:31 +0700

    import react-flip-move

--- MxReactWidget/widget/MxReactWidget.tsx
+++ MxReactWidget/widget/MxReactWidget.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Circle } from "./Circle";
-import { Canvas } from "./Canvas";
+import FlipMove from 'react-flip-move';
 /**
  * Built-in Properties of Mendix
  */
@@ -25,32 +24,42 @@ interface IMxReactWidgetProps extends IMendixDefaultProps {
 }
 /** Widget state, if changed, the widget will be re-rendered */
 interface IWidgetState {
+    data: Array<{
+        key: string;
+        text: string
+    }>
 }
 export default class MxReactWidget extends React.Component<IMxReactWidgetProps, IWidgetState> {
     constructor(props: IMxReactWidgetProps) {
         super(props);
+        this.state = { data: [{ key: "b", text: "asfas" }] };
     }
     componentWillMount() {
         console.log('Component will be mounted');
     }
     componentWillReceiveProps(nextProps: IMxReactWidgetProps) {
         console.log("New context object applied")
+        window.setInterval(() => {
+            this.setState(prevState => ({
+                data: [...prevState.data, { key: "a", text: (new Date()).toDateString() }]
+            }))
+        }, 1000)
+    }
+
+    renderData() {
+        if (this.state && this.state.data) {
+            return this.state.data.map(item => (<li className="list-group-item" key={item.key}>{item.text}</li>))
+        }
+        else {
+            return (<li>Nno data yet</li>)
+        }
     }
     render() {
-        let contextObj = this.props.mxObject;
         return (
             <div>
-                <Canvas>
-                    {contextObj ?
-                        (<Circle xCoord={contextObj.get(this.props.xCoord) as number}
-                            yCoord={contextObj.get(this.props.yCoord) as number}
-                            color={contextObj.get(this.props.color) as string}
-                            radius={contextObj.get(this.props.radius) as number}
-                        />)
-                        :
-                        <div>No context object !</div>
-                    }
-                </Canvas>
+                <FlipMove typeName="ul" className="list-group">
+                    {this.renderData()}
+                </FlipMove>
             </div >
         )
     }
